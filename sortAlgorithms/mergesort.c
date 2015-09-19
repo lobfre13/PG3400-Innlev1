3d9db53efad635ac8c4b27f976b581f7206e9b66
Fredrik Løberg <lobfre13@nith.no>
2015-09-19 02:55:23 +0200

    startet på mergesort

--- sortAlgorithms/mergesort.c
+++ sortAlgorithms/mergesort.c
@@ -1,16 +1,31 @@
 #include <stdio.h>
 #include "../number.h"
+void split(int start, int end, Number *array);
+void merge(int start, int end, Number *array);
 
 void mergesort(int count, Number *array){
-	int min = 0;
-	int max = 2;
-	for(;;){
-		if(array[0].value > array[1].value)
-	}
+	printf("Count: %d\n", count);
+	split(0, count, array);
 }
 
-void split(int count, Number *array){
-	if(count == 1) return;
+void split(int start, int end, Number *array){
+	int size = end - start;
+	printf("Count: %d\t%d\n", start,end);
+	if(size <= 1) return;
 
-	split
+	split(start, start+(size/2), array);
+	split((size/2)+start, end, array);
+	merge(start, end, array);
+
+}
+
+void merge(int start, int end, Number *array){
+	printf("mergingRange: %d\t%d\n", start,end);
+	for(int i = start; i < end; i++){
+		if(array[i].value > array[i+1].value){
+			int tmp = array[i].value;
+			array[i].value = array[i+1].value;
+			array[i+1].value = tmp;
+		}
+	}
 }
