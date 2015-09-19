8041961e23bd0bfbbff702f50faf4afb400c060c
Simen Fonnes <fonsim13@student.westerdals.no>
2015-09-18 20:22:52 -0400

    Implementert bubblesort

--- sortAlgorithms/bubblesort.c
+++ sortAlgorithms/bubblesort.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
 #include "../number.h"
 
-void bubblesort(int count, Number *array){
-	printf("bubble\n");
+void bubblesort(int count, Number *array) {
+	int swap = 1;
+	for (int i = 0; i < count - 1; i++) {
+		if (swap == 1) {
+			swap = 0;
+			for (int j = 0; j < count - 1; j++) {
+				if (array[j] < array[j+1]) {
+					Number tmp = array[j];
+					array[j] = array[j+1];
+					array[j+1] = tmp;
+					swap = 1;
+				} 
+			}
+		} else {
+			return;
+		}
+	}
 }
