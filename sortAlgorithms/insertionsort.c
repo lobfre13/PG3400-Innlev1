4855320a9a7479c137fa21ad0ef85e465fab7540
Simen Fonnes <fonsim13@student.westerdals.no>
2015-09-20 18:27:30 -0400

    Implementert insertion sort

--- sortAlgorithms/insertionsort.c
+++ sortAlgorithms/insertionsort.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 #include "../number.h"
 
-void insertionsort(int count, Number *array){
-	printf("insertion\n");
+void insertionsort(int count, Number *array) {
+	for (int i = 1; i < count; i++) {
+		for (int j = i; j > 0; j--) {
+			if (array[j].value < array[j - 1].value) {
+				int tmp = array[j].value;
+				array[j].value = array[j - 1].value;
+				array[j - 1].value = tmp;
+			} else {
+				break;
+			}
+		}
+	}
 }
