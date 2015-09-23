c2fc0850e5d511f957de7c1f9bb4b4be281eceed
Simen Fonnes <fonsim13@student.westerdals.no>
2015-09-23 06:11:31 -0400

    Begynt å implementere selection sort.

--- sortAlgorithms/selectionsort.c
+++ sortAlgorithms/selectionsort.c
@@ -2,5 +2,15 @@
 #include "../number.h"
 
 void selectionsort(int count, Number *array){
-	printf("selection\n");
+	for (int i = 0; i < count; i++) {
+		Number smallestIndex = array[i];
+		for (int j = i; j < count; j++) {
+			if (array[j].value < smallestIndex.value) {
+				smallestIndex = array[j];
+			} 
+		}
+		Number tmp = array[i];
+		array[i] = smallestIndex;
+		
+	}
 }
